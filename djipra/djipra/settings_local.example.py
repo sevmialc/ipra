b26e82a062593a98707049920a878da8bad3d830
dz <d.zalensky@sevmialc.ru>
2018-05-03 16:40:27 +0300

    Add local settings

---
+++ djipra/djipra/settings_local.example.py
@@ -0,0 +1,11 @@
+
+SECRET_KEY = 'oh my god they killed kenny!'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
