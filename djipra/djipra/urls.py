e55ef879c4d0b2c0f93d810e5f9918863eafe495
dz <d.zalensky@sevmialc.ru>
2018-05-03 17:12:55 +0300

    Title of django admin

--- djipra/djipra/urls.py
+++ djipra/djipra/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = 'ИПРА'
+admin.site.index_title = ''
+admin.site.site_title = 'ИПРА'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
