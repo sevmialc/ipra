79461ba00a54a2774099fb4a226a7ab6bd35cf85
dz <d.zalensky@sevmialc.ru>
2018-05-03 17:38:00 +0300

    Title of vitrina app

---
+++ djipra/vitrina/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'vitrina.apps.VitrinaConfig'
--- djipra/vitrina/apps.py
+++ djipra/vitrina/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class VitrinaConfig(AppConfig):
     name = 'vitrina'
+    verbose_name = 'БД Витрина'
